34714ba2c92a780a25b8638bb7ce45d6447f0e6b
Marina <martiniz1901@gmail.com>
2021-12-07 15:10:47 +0300

    check double

--- src/main/java/org/example/App_1.java
+++ src/main/java/org/example/App_1.java
@@ -3,6 +3,6 @@ package org.example;
 public class App_1 {
     public static void main( String[] args )
     {
-        System.out.println( "check" );
+        System.out.println( "check check" );
     }
 }
